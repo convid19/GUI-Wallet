75e495e8fdee8ff6d8db95b59d513259e8333f87
SpookyPool <vbemailhacker@gmail.com>
2020-03-18 15:01:30 +0100

    GUI Wallet

---
+++ src/js/ws_config.js
@@ -0,0 +1,96 @@
+var config = {};
+
+// self explanatory, your application name, descriptions, etc
+config.appName = 'CoVid-19Wallet';
+config.appDescription = 'CoVid-19 Wallet';
+config.appSlogan = 'Slow and steady wins the race!';
+config.appId = 'xyz.covid19.walletshell';
+config.appGitRepo = 'https://github.com/turtlecoin/turtle-wallet-electron';
+
+// default port number for your daemon (e.g. TurtleCoind)
+config.daemonDefaultRpcPort = 20205;
+
+// wallet file created by this app will have this extension
+config.walletFileDefaultExt = 'twl';
+
+// change this to match your wallet service executable filename
+config.walletServiceBinaryFilename = 'corona-service';
+
+// version on the bundled service (turtle-service)
+config.walletServiceBinaryVersion = "v0.1.0";
+
+// config file format supported by wallet service, possible values:
+// ini -->  for turtle service (or its forks) version <= v0.8.3
+// json --> for turtle service (or its forks) version >= v0.8.4
+config.walletServiceConfigFormat = "json";
+
+// default port number for your wallet service (e.g. turtle-service)
+config.walletServiceRpcPort = 8075;
+
+// block explorer url, the [[TX_HASH]] will be substituted w/ actual transaction hash
+config.blockExplorerUrl = 'http://covid.spookypool.nl/Explorer/?hash=[[TX_HASH]]#blockchain_block';
+
+// default remote node to connect to, set this to a known reliable node for 'just works' user experience
+config.remoteNodeDefaultHost = 'spookypool.nl';
+
+
+// remote node list update url, set to null if you don't have one
+// for TRTL:
+// raw list: https://raw.githubusercontent.com/turtlecoin/turtlecoin-nodes-json/master/turtlecoin-nodes.json
+// filtered: https://trtl.nodes.pub/api/getNodes
+config.remoteNodeListUpdateUrl = '';
+
+// set to false if using raw/unfiltered node list
+config.remoteNodeListFiltered = true;
+
+// fallback remote node list, in case fetching update failed, fill this with known to works remote nodes
+config.remoteNodeListFallback = [
+  'spookypool.nl:20205'
+];
+config.remoteNodeCacheSupported = false;
+config.remoteNodeSslSupported = false;
+
+// your currency name
+config.assetName = 'CoVid-19';
+// your currency ticker
+config.assetTicker = 'CoVid-19';
+// your currency address prefix, for address validation
+config.addressPrefix = 'cv19';
+// standard wallet address length, for address validation
+config.addressLength = 99;
+// integrated wallet address length, for address validation. Added length is length of payment ID encoded in base58.
+config.integratedAddressLength = config.addressLength + ((64 * 11) / 8);
+
+// minimum fee for sending transaction
+config.minimumFee = 0.1;
+// minimum amount for sending transaction
+config.mininumSend = 0.1;
+// default mixin/anonimity for transaction
+config.defaultMixin = 0;
+// to represent human readable value
+config.decimalPlaces = 5;
+// to convert from atomic unit
+config.decimalDivisor = 100000 ** config.decimalPlaces;
+
+// obfuscate address book entries, set to false if you want to save it in plain json file.
+// not for security because the encryption key is attached here
+config.addressBookObfuscateEntries = true;
+// key use to obfuscate address book contents
+config.addressBookObfuscationKey = '79009fb00ca1b7130832b42de45142ff6c4b7f333fe6fba5';
+// initial/sample entries to fill new address book
+config.addressBookSampleEntries = [
+  {
+    name: 'CoVid-19 Donation',
+    address: 'cv19vP8p9hBDNFYxrUZYjHC9a8pUkWTXxNPE3jjR6iXjHHHBN6Sd2mAKyX9QHwfKRiQJXuE6KZFMYfZyDBLxC9FcMuSmLj67WNS',
+    paymentId: '',
+  }
+];
+// cipher config for private address book
+config.addressBookCipherConfig = {
+  algorithm: 'aes-256-gcm',
+  saltLenght: 128,
+  pbkdf2Rounds: 10000,
+  pbkdf2Digest: 'sha512'
+};
+
+module.exports = config;
